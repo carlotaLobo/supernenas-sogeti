5ec442bb75cdfcc8d32faad4d59bd3bb48fa73d1
carlotalobo <carlota.lobo.sanz@gmail.com>
2020-11-29 18:25:55 +0100

    buscador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { Login2Component } from './components/login2/login2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    Login2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    var pathEls = document.querySelectorAll('path');
+    for (var i = 0; i < pathEls.length; i++) {
+      var pathEl = pathEls[i];
+      var offset = anime.setDashoffset(pathEl);
+      pathEl.setAttribute('stroke-dashoffset', offset);
+      anime({
+        targets: pathEl,
+        strokeDashoffset: [offset, 0],
+        duration: anime.random(1000, 3000),
+        delay: anime.random(0, 2000),
+        loop: true,
+        direction: 'alternate',
+        easing: 'easeInOutSine',
+        autoplay: true,
+      });
+    }
+  }
+}
